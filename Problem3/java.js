13f5b407e9a3e1f99ef701c239bc77894f950745
JohnLRanola <johnlranola@gmail,com>
2023-10-30 22:03:24 +0000

    Added js for the api

---
+++ Problem3/java.js
@@ -0,0 +1,37 @@
+function getInfo() {
+    const username = document.getElementById('username').value;
+    const apiUrl = `https://api.github.com/users/${username}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const userInfo = document.getElementById('user');
+            userInfo.innerHTML = `
+                <img src="${data.avatar_url}" alt="Avatar">
+                <h2>${data.login}</h2>
+                <p>Name: ${data.name || 'N/A'}</p>
+                <p>Username: ${data.login}</p>
+                <p>Email: ${data.email || 'N/A'}</p>
+                <p>Location: ${data.location || 'N/A'}</p>
+                <p>Number of Gists: ${data.public_gists}</p>
+                <h3>Repositories:</h3>
+            `;
+
+            const reposList = document.getElementById('repos');
+            fetch(data.repos_url)
+                .then(response => response.json())
+                .then(repos => {
+                    repos.slice(0, 5).forEach(repo => {
+                        const li = document.createElement('li');
+                        li.textContent = `${repo.name} - ${repo.description || 'No description was provided'}`;
+                        reposList.appendChild(li);
+                    });
+                });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            const userInfo = document.getElementById('user');
+            userInfo.innerHTML = 'Error: User not available.';
+            document.getElementById('repos-list').innerHTML = '';
+        });
+}
