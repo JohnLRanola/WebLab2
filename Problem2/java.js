8f06fff1596b402a6009af26665746aa74aa1830
JohnLRanola <johnlranola@gmail,com>
2023-10-30 21:35:39 +0000

    Added console log to be more readable

--- Problem2/java.js
+++ Problem2/java.js
@@ -3,9 +3,10 @@ fetch('http://jsonplaceholder.typicode.com/posts')
   .then(posts => {
     const longTitles = posts
       .map(post => post.title)
-
-      .filter(title => title.split(' ').length > 6);
-    console.log("The posts have more than 6 words:")
+      .filter(title => title.split(' ').length > 6)
+      .join('\n'); 
+      
+    console.log("The posts have more than 6 words:");
     console.log(longTitles);
   })
   .catch(error => console.error(error));
