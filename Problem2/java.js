1b60286d2261f5f5c5e34d30d30c421503b5cea5
JohnLRanola <johnlranola@gmail,com>
2023-10-30 21:22:40 +0000

    Added html to connect js and fixed output for q1

--- Problem2/java.js
+++ Problem2/java.js
@@ -3,9 +3,9 @@ fetch('http://jsonplaceholder.typicode.com/posts')
   .then(posts => {
     const longPostTitles = posts
       .map(post => post.title)
+      
       .filter(title => title.split(' ').length > 6);
-
-    console.log(longPostTitles);
     console.log("The posts have more than 6 words:")
+    console.log(longPostTitles);
   })
   .catch(error => console.error(error));
