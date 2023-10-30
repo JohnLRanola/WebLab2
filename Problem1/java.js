b1db48b72e70cd815c68d98e76d804c63665e4b2
JohnLRanola <johnlranola@gmail,com>
2023-10-30 20:21:55 +0000

    Added a validate function

--- Problem1/java.js
+++ Problem1/java.js
@@ -1,6 +1,6 @@
 let insertHeaderRow = true;
 
-function createEntry() {
+function createContact() {
     let isValid = validate();
     if (isValid) {
         if (insertHeaderRow) {
@@ -24,6 +24,7 @@ function createEntry() {
     }
 }
 
+
 function validate() {
     let name = document.getElementById('name').value.trim();
     let phone = document.getElementById('phone').value.trim();
@@ -35,57 +36,11 @@ function validate() {
     let phoneRegex = /^[0-9]+$/;
     let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    if (name === '' || phone === '' || email === '' || !name.match(nameRegex) || name.length > 20 || !phone.match(phoneRegex) || phone.length != 10 || !email.match(emailRegex) || email.length > 40) {
+    if (name === '' || phone === '' || email === '' || !name.match(nameRegex) || name.length > 20 || !phone.match(phoneRegex) || phone.length != 10 || !email.match(emailRegex) || email.length > 100) {
         errorMessage.style.display = "block";
         return false;
     }
 
     errorMessage.style.display = "hidden";
     return true;
-}
-
-function createHeaders() {
-    let tableBody = document.getElementById('myTable');
-    let row = tableBody.insertRow(-1);
-    row.insertCell(-1).innerHTML = "<b>Name</b>";
-    row.insertCell(-1).innerHTML = "<b>Phone</b>";
-    row.insertCell(-1).innerHTML = "<b>Email</b>";
-}
-
-function filter(sortByElement) {
-    let tableBody = document.getElementById("myTable");
-    let rows = Array.from(tableBody.rows).slice(1); 
-    rows.sort((a, b) => {
-        let aText = a.cells[sortByElement].textContent.toLowerCase();
-        let bText = b.cells[sortByElement].textContent.toLowerCase();
-        return aText.localeCompare(bText);
-    });
-
-    tableBody.innerHTML = "";
-    tableBody.appendChild(tableBody.rows[0]); 
-    rows.forEach(row => tableBody.appendChild(row));
-}
-
-function filterName() {
-    filter(0);
-}
-
-function filterPhone() {
-    filter(1);
-}
-
-function telSearch() {
-    let input = document.getElementById("myInput").value.trim().toUpperCase();
-    let tableBody = document.getElementById("myTable");
-    let rows = tableBody.rows;
-
-    for (let index = 1; index < rows.length; index++) {
-        let text = rows[index].getElementsByTagName("TD")[1];
-        let txtValue = text.textContent || text.innerText;
-        if (txtValue.toUpperCase().indexOf(input) > -1) {
-            rows[index].style.display = "";
-        } else {
-            rows[index].style.display = "none";
-        }
-    }
 }
